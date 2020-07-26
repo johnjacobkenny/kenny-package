f84777e0a05ec8c052f3d7b7e4780c04b2fc97a9
Kenny John Jacob <johnjacobkenny@gmail.com>
2020-07-26 07:21:58 +0530

    feat(multiply): add multiply functionality

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,7 @@ export const add = (a: number, b: number) => {
 export const subtract = (a: number, b: number) => {
   return a - b;
 };
+
+export const multiply = (a: number, b: number) => {
+  return a * b;
+};
