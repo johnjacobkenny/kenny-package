282ad01f639274e0702a17aa83f9322cd140837e
Kenny John Jacob <johnjacobkenny@gmail.com>
2020-07-26 07:22:24 +0530

    test(multiply): add test for multiply

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,12 +1,19 @@
-import { add, subtract } from "../src/index";
+import { add, multiply, subtract } from "../src/index";
 
 describe("add", () => {
   it("should add two positive numbers", () => {
     expect(add(1, 2)).toBe(3);
   });
 });
+
 describe("subtract", () => {
   it("should subtract two positive numbers", () => {
     expect(subtract(5, 2)).toBe(3);
+  });
+});
+
+describe("mulitply", () => {
+  it("should multiply two positive numbers", () => {
+    expect(multiply(5, 2)).toBe(10);
   });
 });
