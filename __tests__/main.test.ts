7626a9e7daa3e8be6c4e17129e43c1251d467d63
Kenny John Jacob <johnjacobkenny@gmail.com>
2020-07-26 06:47:06 +0530

    test(subtract): add test for subtract module

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,7 +1,12 @@
-import { add } from "../src/index";
+import { add, subtract } from "../src/index";
 
 describe("add", () => {
   it("should add two positive numbers", () => {
     expect(add(1, 2)).toBe(3);
+  });
+});
+describe("subtract", () => {
+  it("should subtract two positive numbers", () => {
+    expect(subtract(5, 2)).toBe(3);
   });
 });
